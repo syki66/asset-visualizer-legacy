85c963e7de8dfcfd16a30236513648e7e461e53b
syki66 <66syki@gmail.com>
2021-11-19 02:39:24 +0900

    update calculatePrin func

--- SHinvt_TXN_Hx_visualizer.py
+++ SHinvt_TXN_Hx_visualizer.py
@@ -1,8 +1,9 @@
 # 신한알파 MTS -> 자산현황 -> [1750] 계좌별 거래내역(원장) -> 기간선택 후 조회 -> 마우스 오른쪽 버튼 -> 엑셀로 내보내기 -> CSV
 
-# 배당금 , 원금(보정), 배당율, 수익률, 환율, 그래프 추이 한번 그려주고, etf 보유종목 현황이나 비율 정도.
+# 배당금 , 배당율, 수익률, 환율, 그래프 추이 한번 그려주고, etf 보유종목 현황이나 비율 정도.
 # 공모주 여부 선택, 보유주식 잔고, 평가잔고, 보유종목 top10 계좌 여러개 보여주고 수익률 등 합산 가능하게, 거래내역 두개 비교해서 전산누락도 확인
-# 외화 평단, 예금과 수익률 비교
+# 예금과 수익률 비교, 기간, 파일이름을 계좌명으로 출력
+# 현재 수수료율
 
 import csv
 from datetime import datetime
@@ -33,8 +34,7 @@ def parseData(list):
 
     return array
 
-# 결과에 대한 보정값이나 특정 시점에 기준값을 받아서 계산하는 방식으로하기
-def calculatePrin(list, principal=0, correction=0):
+def calculatePrin(list, start_date, end_date, principal, correction):
     '''투입 원금 계산기'''
     deposit_kw = ['은행이체입금', '(펌뱅킹)입금']
     withdraw_kw = ['은행이체출금', '(펌뱅킹)출금', '체크카드승인']
@@ -42,9 +42,9 @@ def calculatePrin(list, principal=0, correction=0):
     start = 0
     end = len(list)
     for i in range(len(list)):
-        if datetime(2021, 10, 15) > datetime(*list[i][0]):
+        if datetime(*start_date) > datetime(*list[i][0]):
             start = i + 1
-        if datetime(2021, 11, 18) >= datetime(*list[i][0]):
+        if datetime(*end_date) >= datetime(*list[i][0]):
             end = i
 
     d_sum = 0
@@ -58,10 +58,26 @@ def calculatePrin(list, principal=0, correction=0):
             if list[i][1].endswith(wkw):
                 w_sum += int(list[i][4].replace(',', ''))
 
-    print(f'입금고액 : {d_sum:,}원')
-    print(f'출금고액 : {w_sum:,}원')
-    print(f'투자 원금 : {d_sum - w_sum + principal + correction:,}원')
+    principal_result = d_sum - w_sum + principal + correction
+
+    return d_sum, w_sum, principal_result
+
+
 
 test = readCSV('1111.csv')
-calculatePrin(parseData(test), 0)
+array = parseData(test)
+t1, t2, t3 = calculatePrin(array, (2021, 10, 15), (2021, 11, 18), 0, 0)
 
+aaaa = []
+for i in array:
+    aaaa.append(i[1])
+print(set(aaaa))
+
+
+print(f'입금고액 : {t1:,}원')
+print(f'출금고액 : {t2:,}원')
+print(f'투자 원금 : {t3:,}원')
+
+
+# 주식수 계산은 타사대체입고, 장내매수, 해외증권해외주식매수, 해외증권해외주식매도, 장내매도, 계좌대체입고
+# 남은 원화는 가장 최근의 원화 최종금액, 외화는 rp 등 고려해서 계산되나
