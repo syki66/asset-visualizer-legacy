bdf8826c96ed34e2cbcc8f62b92f2e0d1275b86e
syki66 <66syki@gmail.com>
2021-11-25 03:04:11 +0900

    move func into class

--- SHinvt_TXN_Hx_visualizer.py
+++ SHinvt_TXN_Hx_visualizer.py
@@ -19,7 +19,7 @@ def readCSV(file):
 
     return array
 
-def preprocessData(list):
+def preprocessData(list, start_date, end_date):
     '''데이터 전처리 함수'''
     array = []
     for i in range(0, len(list), 2):
@@ -33,64 +33,91 @@ def preprocessData(list):
     if datetime(*array[0][0]) > datetime(*array[-1][0]):
         array.reverse()
 
-    return array
-
-def calculateDW(list, start_date, end_date):
-    '''입금고액, 출금고액 계산 함수'''
-    deposit_keyword = ['은행이체입금', '(펌뱅킹)입금']
-    withdraw_keyword = ['은행이체출금', '(펌뱅킹)출금', '체크카드승인']
-
     start = 0
-    end = len(list)
-    for i in range(len(list)):
-        if datetime(*start_date) > datetime(*list[i][0]):
+    end = len(array)
+    for i in range(len(array)):
+        if datetime(*start_date) > datetime(*array[i][0]):
             start = i + 1
-        if datetime(*end_date) >= datetime(*list[i][0]):
+        if datetime(*end_date) >= datetime(*array[i][0]):
             end = i
 
-    d_sum = 0
-    w_sum = 0
-    for i in range(start, end + 1):
-        for dkw in deposit_keyword:
-            if list[i][1].endswith(dkw):
-                d_sum += int(list[i][4].replace(',', ''))
-                
-        for wkw in withdraw_keyword:
-            if list[i][1].endswith(wkw):
-                w_sum += int(list[i][4].replace(',', ''))
+    return array[start : end]
+
+class Calculator:
+    def __init__(self, list):
+        self.list = list
+
+    def calculateDW(self):
+        '''입금고액, 출금고액 계산 함수'''
+        deposit_keyword = ['은행이체입금', '(펌뱅킹)입금']
+        withdraw_keyword = ['은행이체출금', '(펌뱅킹)출금', '체크카드승인']
+
+        deposit = 0
+        withdraw = 0
+        for line in self.list:
+            for dkw in deposit_keyword:
+                if line[1].endswith(dkw):
+                    deposit += int(line[4].replace(',', ''))
+                    
+            for wkw in withdraw_keyword:
+                if line[1].endswith(wkw):
+                    withdraw += int(line[4].replace(',', ''))
+        
+        return deposit, withdraw
+
+    def calculatePrin(self, principal, correction):
+        deposit, withdraw = self.calculateDW()
+        principal_result = (deposit - withdraw) + (principal + correction)
+
+        return principal_result
+
     
-    return d_sum, w_sum
+    # def calculateUSD(list):
+    #     '''달러 예수금 (달러 소수점 절사)'''
+    #     for i in range(len(list) - 1, -1, -1):
+    #         if list[i][2] == 'USD' or list[i][22] == 'USD':
+    #             if list[i][23] == '':
+    #                 return 0
+    #             else:
+    #                 return int(list[i][23].replace(',', ''))
 
-def calculatePrin(deposit, withdraw, principal, correction):
-    principal_result = (deposit - withdraw) + (principal + correction)
-
-    return principal_result
-
-def calculateUSD(list):
-    '''달러 예수금 (달러 소수점 절사)'''
-    for i in range(len(list) - 1, -1, -1):
-        if list[i][2] == 'USD' or list[i][22] == 'USD':
-            if list[i][23] == '':
-                return 0
-            else:
-                return int(list[i][23].replace(',', ''))
-            
-
-
-def calculateKRW(list):
-    '''원화 잔고'''
-    pass
+    # # def test(list):
+    # #     in_kw = ['해외배당금', '외화RP매도입금', '은행이체외화입금']
+    # #     out_kw = ['외국납부세액', '외화RP매수출금', ]
+    # #     return 0
+                
+    # def calculateUSDRP(list):
+    #     '''달러 RP 잔고 계산'''
+    #     for i in range(len(list)):
+    #         pass
 
 
+    # '환전입금', '환전출금','외화RP재투자환매', '외화RP재투자매수', '외화매수환전', 
+    # def calculateKRW(list):
+    #     '''원화 잔고'''
+    #     pass
 
 
-rawData = readCSV('1111.csv')
-data = preprocessData(rawData)
 
-deposit, withdraw = calculateDW(data, (2021, 10, 15), (2021, 11, 18))
-principal = calculatePrin(deposit, withdraw, 0, 0)
 
-USD = (calculateUSD(data))
+
+# test = Calculator([10,20], 30, 40)
+
+
+rawArray = readCSV('1111.csv')
+data = preprocessData(rawArray,(2021, 10, 15), (2021, 11, 18))
+
+cal = Calculator(data)
+# cal.calculateDW()
+print(cal.calculatePrin(0, 0))
+
+
+
+# deposit, withdraw = calculateDW(data, (2021, 10, 15), (2021, 11, 18))
+# principal = calculatePrin(deposit, withdraw, 0, 0)
+
+# # USD = (calculateUSD(data))
+# USD = (test(data))
 
 # aaaa = []
 # for i in data:
@@ -98,11 +125,11 @@ USD = (calculateUSD(data))
 # print(set(aaaa))
 
 
-print(f'입금고액 : {deposit:,}원')
-print(f'출금고액 : {withdraw:,}원')
-print(f'투자 원금 : {principal:,}원')
+# print(f'입금고액 : {deposit:,}원')
+# print(f'출금고액 : {withdraw:,}원')
+# print(f'투자 원금 : {principal:,}원')
 
-print(f'달러 예수금 : {USD}$')
+# print(f'달러 예수금 : {USD}$')
 
 
 # 주식수 계산은 타사대체입고, 장내매수, 해외증권해외주식매수, 해외증권해외주식매도, 장내매도, 계좌대체입고
